ecb6325993db87a96ff8888c50999291218b3f9f
vinay <vinaykishore2512@gmail.com>
2025-01-23 00:20:19 +0530

    Portfolio 1.0

--- README.md
+++ README.md
@@ -18,25 +18,25 @@ This web page was made with the following technologies:
 This project contains 6 sections: **Home**, **About**, **Services**, **Work**, **Testimonials** and **Contact**.
 
 ### Home
-#### In that home section, there is a title and description of the web page, and to the right of it an image. On the right side there is a navigation bar that will take you to different sections of the website. And at the top right you will find the social networks.
-![preview home](public/home-preview.jpeg)
+#### In that home section, On the right side there is a navigation bar that will take you to different sections of the website. And at the top right you will find the social networks.
+![preview home](public/home-preview.png)
  
 ### About
-#### In that other section, you will see a title and a description. Below that you will see information such as years of experience, number of satisfied clients, completed projects, and awards won. On the right, you will see skills, awards, experience and credentials.
-![preview about](public/about-preview.jpeg)
+#### In that other section, you will see a title and a description. Below that you will see information such as Comprehensive expertise, Certified proficiencies, Finished projects, and awards won. On the right, you will see skills, awards, experience, credentials and education.
+![preview about](public/about-preview.png)
 
 ### Services
 #### In this services section, there is a title and a short description. And on the right side the services offered to the client.
-![preview services](public/services-preview.jpeg)
+![preview services](public/services-preview.png)
 
 ### Work
-#### In this other section, you will see a title and a short description. Additionally, on the right side you will see the completed projects.
-![preview work](public/work-preview.jpeg)
+#### In this other section, you will see a title and a short description. Additionally, on the right side you will see the completed projects on various domains.
+![preview work](public/work-preview.png)
 
 ### Testimonials
-#### In this section, you will see testimonials from satisfied customers.
-![preview testimonials](public/testimonials-preview.jpeg)
+#### In this section, you will see testimonials from mentors.
+![preview testimonials](public/testimonials-preview.png)
 
 ### Contact
 #### In this other section, you will see a form which you can fill out to send a message and get in touch.
-![preview contact](public/contact-preview.jpeg)
+![preview contact](public/contact-preview.png)
--- src/components/TestimonialSlider.js
+++ src/components/TestimonialSlider.js
@@ -1,25 +1,25 @@
 // testimonial data
 const testimonialData = [
   {
-    image: '/t-avt-1.png',
-    name: 'Durga Sai Prasad',
-    position: 'Coding Mentor',
+    image: "/dsp.png",
+    name: "Durga Sai Prasad",
+    position: "Coding Mentor",
     message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+    "As a coding mentor, he demonstrates strong problem-solving skills and a passion for continuous learning. He effectively simplifies complex coding challenges, helping others grasp key concepts. With contributions to projects like the Resume Builder and Veda Website, he guides others toward improving their technical skills and consistently delivering high-quality solutions."
   },
   {
-    image: '/t-avt-2.png',
-    name: 'Hanumanthu Buddha',
-    position: 'Development Mentor',
+    image: "/hanumanthu.png",
+    name: "Hanumanthu Buddha",
+    position: "Development Mentor",
     message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+      "As a mentor, he excels in problem-solving and continuous learning. With a knack for simplifying complex concepts, he has made valuable contributions to projects like the Resume Builder and Veda Website. He fosters growth in others, guiding teams toward technical excellence and consistently delivering high-quality results.",
   },
   {
-    image: '/t-avt-3.png',
-    name: 'Kiran Kumar',
-    position: 'Communication Mentor',
+    image: "/Kiran Kumar.png",
+    name: "Kiran Kumar",
+    position: "Communication Mentor",
     message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+      "As a communication mentor, he excels in conveying ideas clearly and effectively. He helps others improve their communication skills, fostering a collaborative environment where team members feel empowered to share their thoughts. With a focus on clarity and understanding, he guides others to enhance both verbal and written communication",
   },
 ];
 
@@ -27,9 +27,9 @@ const testimonialData = [
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 // Required modules
 import { Navigation, Pagination } from "swiper";
@@ -45,52 +45,51 @@ const TestimonialSlider = () => {
     <Swiper
       navigation={true}
       pagination={{
-        clickable: true
+        clickable: true,
       }}
       modules={[Navigation, Pagination]}
       className="h-[400px]"
     >
-      {
-        testimonialData.map((person, index) => {
-          return (
-            <SwiperSlide key={index}>
-              <div className="flex flex-col items-center md:flex-row gap-x-8 h-full px-16">
-                {/* Avatar, name, position */}
-                <div className="w-full max-w-[300px] flex flex-col xl:justify-center items-center relative mx-auto xl:mx-0">
-                  <div className="flex flex-col justify-center text-center">
-                    {/* Avatar */}
-                    <div className="mb-2 mx-auto">
-                      <Image src={person.image} width={100} height={100} alt="" />
-                    </div>
-                    {/* Name */}
-                    <div className="text-lg">
-                      {person.name}
-                    </div>
-                    {/* Position */}
-                    <div className="text-[12px] uppercase font-extralight tracking-widest">
-                      {person.position}
-                    </div>
+      {testimonialData.map((person, index) => {
+        return (
+          <SwiperSlide key={index}>
+            <div className="flex flex-col items-center md:flex-row gap-x-8 h-full px-16">
+              {/* Avatar, name, position */}
+              <div className="w-full max-w-[300px] flex flex-col xl:justify-center items-center relative mx-auto xl:mx-0">
+                <div className="flex flex-col justify-center text-center">
+                  {/* Avatar */}
+                  <div
+                    className="mb-2 mx-auto rounded-circle overflow-hidden"
+                    style={{ width: "100px", height: "100px", borderRadius:"50px" }}
+                  >
+                    <Image src={person.image} width={100} height={100} alt="" />
                   </div>
-                </div>
-                {/* Quote & Message */}
-                <div className="flex-1 flex flex-col justify-center before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0 xl:before:h-[200px] relative xl:pl-20">
-                  {/* Quote Icon */}
-                  <div className="mb-4">
-                    <FaQuoteLeft className="text-4xl xl:text-6xl text-white/20 mx-auto md:mx-0" />
-                  </div>
-                  {/* Message */}
-                  <div className="xl:text-lg text-center md:text-left">
-                    {person.message}
+
+                  {/* Name */}
+                  <div className="text-lg">{person.name}</div>
+                  {/* Position */}
+                  <div className="text-[12px] uppercase font-extralight tracking-widest">
+                    {person.position}
                   </div>
                 </div>
               </div>
-            </SwiperSlide>
-          )
-        })
-      }
+              {/* Quote & Message */}
+              <div className="flex-1 flex flex-col justify-center before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0 xl:before:h-[200px] relative xl:pl-20">
+                {/* Quote Icon */}
+                <div className="mb-4">
+                  <FaQuoteLeft className="text-4xl xl:text-6xl text-white/20 mx-auto md:mx-0" />
+                </div>
+                {/* Message */}
+                <div className="xl:text-lg text-center md:text-left">
+                  {person.message}
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
     </Swiper>
   );
 };
 
 export default TestimonialSlider;
-
